32daf0bad2c8ad1f796499c0aa7ca1e33b92f0b1
rane-hs <rane.hs@gmail.com>
2018-05-08 14:30:25 +0900

    add support pythonnet2.2.2 for python3.7

--- setup.py
+++ setup.py
@@ -355,6 +355,7 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Operating System :: Microsoft :: Windows',
             'Operating System :: POSIX :: Linux',
             'Operating System :: MacOS :: MacOS X',
